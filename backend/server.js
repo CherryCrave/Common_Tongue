08e14353baa94796a93fdffae3aa752571df9bab
CherryCrave <bbellenie13@gmail.com>
2026-01-25 23:00:21 +0000

    Set up of Express framework and commencement of server

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Allows the React Native frontend to communicate with the backend.
+app.use(cors());
+app.use(express.json());
+
+// Starts the local server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:` + PORT);
+});
