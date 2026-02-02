0ac5b3a663ed023f5005f28511948507319ae8fb
CherryCrave <bbellenie13@gmail.com>
2026-02-02 15:41:06 +0000

    Creation of user auth routing

---
+++ backend/routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const pool = require('../config/database');
+const router = express.Router();
+
+// User registration functionality
+
+// User login functionality
+
+module.exports = router;
