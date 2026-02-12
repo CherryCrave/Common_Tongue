4b49f8b9ced9af812cadc72c70fa905997f025b9
CherryCrave <bbellenie13@gmail.com>
2026-02-12 14:42:20 +0000

    Forgot to add lessonFeedback routing setup

--- backend/routes/lessonFeedback.js
+++ backend/routes/lessonFeedback.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const pool = require('../config/database');
 const router = express.Router();
